8b333a831098b34c5e907d90d5d2cf7a66050b33
aixion1506 <aixion1506@gmail.com>
2024-12-09 15:32:44 +0900

    Study: Nest Cli로 module, controller 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesController } from './messages/messages.controller';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [MessagesModule],
+  controllers: [AppController, MessagesController],
   providers: [AppService],
 })
 export class AppModule {}
