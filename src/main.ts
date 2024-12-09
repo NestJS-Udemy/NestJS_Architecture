64d878a6851bda192838b08f1d4fd790cc3ac2fb
aixion1506 <aixion1506@gmail.com>
2024-12-09 14:30:08 +0900

    Refacor: NestJS Module, Controller

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,5 @@
-import { Controller, Module, Get } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return "hi rani!";
-  }
-}
-
-@Module({
-  controllers: [AppController],
-})
-class AppMoule {}
+import { AppMoule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppMoule);
