4de0bf37695c40488cd3d2f3fa992c76cbe32b56
aixion1506 <aixion1506@gmail.com>
2024-12-10 16:58:52 +0900

    Study: Pipe와 DTO를 이용한 Validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(configService.get<number>('BACKEND_PORT'));
 }
